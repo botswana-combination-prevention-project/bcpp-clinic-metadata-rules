61d8b0de8fb004369279eb8b276f3c42e0ca147d
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-18 21:28:25 +0200

    update rule groups

--- bcpp_clinic_metadata_rules/metadata_rules/questionnaire_rule_group.py
+++ bcpp_clinic_metadata_rules/metadata_rules/questionnaire_rule_group.py
@@ -1,10 +1,10 @@
-from bcpp_clinic_labs.labs import panel_rbd, panel_vl
+from bcpp_clinic_labs.labs import panel_vl
 from edc_constants.constants import OTHER
 from edc_metadata.constants import NOT_REQUIRED, REQUIRED
 from edc_metadata.rules import CrfRule, RequisitionRule
 from edc_metadata.rules.crf import CrfRuleGroup
 from edc_metadata.rules.decorators import register
-from edc_metadata.rules.predicate import P
+from edc_metadata.rules.predicate import P, PF
 from edc_metadata.rules.requisition import RequisitionRuleGroup
 
 from ..constants import MASA_VL_SCHEDULED, INITIATION
@@ -21,18 +21,6 @@ class QuestionnaireCrfRuleGroup(CrfRuleGroup):
         alternative=NOT_REQUIRED,
         target_models=[f'{app_label}.viralloadtracking'])
 
-    viralloadtracking1 = CrfRule(
-        predicate=P('registration_type', 'eq', OTHER),
-        consequence=NOT_REQUIRED,
-        alternative=REQUIRED,
-        target_models=[f'{app_label}.viralloadtracking'])
-
-    viralloadtracking2 = CrfRule(
-        predicate=P('registration_type', 'eq', INITIATION),
-        consequence=NOT_REQUIRED,
-        alternative=REQUIRED,
-        target_models=[f'{app_label}.viralloadtracking'])
-
     class Meta:
         source_model = f'{app_label}.questionnaire'
         app_label = 'bcpp_clinic_metadata_rules'
@@ -42,9 +30,11 @@ class QuestionnaireCrfRuleGroup(CrfRuleGroup):
 class QuestionnaireRequisitionRuleGroup(RequisitionRuleGroup):
 
     initiation = RequisitionRule(
-        predicate=P('registration_type', 'eq', MASA_VL_SCHEDULED),
-        consequence=NOT_REQUIRED,
-        alternative=REQUIRED,
+        predicate=PF(
+            'registration_type',
+            func=lambda x: True if x in [INITIATION, OTHER] else False),
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
         target_panels=[panel_vl],)
 
     class Meta:
