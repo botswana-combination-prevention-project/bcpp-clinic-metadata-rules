a87112ade05f50e6fc4e329f11a9b49bf1190c57
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-08-18 08:21:54 +0200

    update rule groups

--- bcpp_clinic_metadata_rules/apps.py
+++ bcpp_clinic_metadata_rules/apps.py
@@ -6,35 +6,8 @@ class AppConfig(DjangoApponfig):
     name = 'bcpp_clinic_metadata_rules'
 
 
-if 'bcpp_clinic_metadata_rules' in settings.APP_NAME:
-    from edc_protocol.apps import AppConfig as BaseEdcProtocolAppConfig, SubjectType, Cap
-    from datetime import datetime
-    from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
-    from edc_appointment.facility import Facility
-    from dateutil.tz import gettz
+if settings.APP_NAME == 'bcpp_clinic_metadata_rules':
     from edc_metadata.apps import AppConfig as MetadataAppConfig
 
     class EdcMetadataAppConfig(MetadataAppConfig):
-        reason_field = {'bcpp_clinic_subject.subjectvisit': 'reason'}
-
-    class EdcProtocolAppConfig(BaseEdcProtocolAppConfig):
-        protocol = 'BHP066'
-        protocol_number = '066'
-        protocol_name = 'BCPP Clinic'
-        protocol_title = 'Botswana Combination Prevention Project'
-        subject_types = [
-            SubjectType('subject', 'Research Subject',
-                        Cap(model_name='bcpp_clinic_subject.subjectconsent', max_subjects=9999)),
-        ]
-        study_open_datetime = datetime(
-            2013, 10, 18, 0, 0, 0, tzinfo=gettz('UTC'))
-        study_close_datetime = datetime(
-            2018, 12, 1, 0, 0, 0, tzinfo=gettz('UTC'))
-
-        @property
-        def site_name(self):
-            return 'test_community'
-
-        @property
-        def site_code(self):
-            return '01'
+        reason_field = {'bcpp_clinic_metadata_rules.subjectvisit': 'reason'}
--- bcpp_clinic_metadata_rules/metadata_rules/questionnaire_rule_group.py
+++ bcpp_clinic_metadata_rules/metadata_rules/questionnaire_rule_group.py
@@ -1,13 +1,13 @@
-from edc_constants.constants import OTHER, YES
+from bcpp_clinic_labs.labs import panel_rbd, panel_vl
+from edc_constants.constants import OTHER
 from edc_metadata.constants import NOT_REQUIRED, REQUIRED
 from edc_metadata.rules import CrfRule, RequisitionRule
 from edc_metadata.rules.crf import CrfRuleGroup
 from edc_metadata.rules.decorators import register
-from edc_metadata.rules.predicate import P, PF
+from edc_metadata.rules.predicate import P
 from edc_metadata.rules.requisition import RequisitionRuleGroup
-from bcpp_clinic_labs.labs import panel_vl
 
-from .constants import INITIATION, MASA_VL_SCHEDULED
+from ..constants import MASA_VL_SCHEDULED, INITIATION
 
 app_label = 'bcpp_clinic_subject'
 
@@ -21,37 +21,39 @@ class QuestionnaireCrfRuleGroup(CrfRuleGroup):
         alternative=NOT_REQUIRED,
         target_models=[f'{app_label}.viralloadtracking'])
 
+    viralloadtracking1 = CrfRule(
+        predicate=P('registration_type', 'eq', OTHER),
+        consequence=NOT_REQUIRED,
+        alternative=REQUIRED,
+        target_models=[f'{app_label}.viralloadtracking'])
+
+    viralloadtracking2 = CrfRule(
+        predicate=P('registration_type', 'eq', INITIATION),
+        consequence=NOT_REQUIRED,
+        alternative=REQUIRED,
+        target_models=[f'{app_label}.viralloadtracking'])
+
     class Meta:
-        app_label = 'bcpp_clinic_metadata_rules'
         source_model = f'{app_label}.questionnaire'
+        app_label = 'bcpp_clinic_metadata_rules'
 
 
 @register()
 class QuestionnaireRequisitionRuleGroup(RequisitionRuleGroup):
 
     initiation = RequisitionRule(
-        predicate=PF(
-            'registration_type',
-            func=lambda x: True if x in [INITIATION, OTHER] else False),
+        predicate=P('registration_type', 'eq', MASA_VL_SCHEDULED),
         consequence=REQUIRED,
         alternative=NOT_REQUIRED,
+        target_panels=[panel_rbd],)
+
+    initiation = RequisitionRule(
+        predicate=P('registration_type', 'eq', MASA_VL_SCHEDULED),
+        consequence=NOT_REQUIRED,
+        alternative=REQUIRED,
         target_panels=[panel_vl],)
 
     class Meta:
-        app_label = 'bcpp_clinic_metadata_rules'
         source_model = 'bcpp_clinic_subject.questionnaire'
         requisition_model = f'{app_label}.subjectrequisition'
-
-
-@register()
-class ViralLoadTrackingCrfRuleGroup(CrfRuleGroup):
-
-    is_drawn = CrfRule(
-        predicate=P('is_drawn', 'eq', YES),
-        consequence=REQUIRED,
-        alternative=NOT_REQUIRED,
-        target_models=[f'{app_label}.vlresult'])
-
-    class Meta:
         app_label = 'bcpp_clinic_metadata_rules'
-        source_model = f'{app_label}.viralloadtracking'
